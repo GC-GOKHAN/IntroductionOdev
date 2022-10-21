b2404036c1f2fc60dfbf0a7850309d3ad020b4b6
Gökhan ÇALIŞKAN <gokhancaliska_n@hotmail.com>
2022-10-22 00:41:38 +0300

    Odev4

---
+++ Odev4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Odev4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Kullanıcıdan kullanıcı adı ve şifre isteyelim. Kullanıcı Adı => Admin && Şifre => 123 , Eğer kullanıcı yanlış girerse bu bilgileri tekrar isteyelim.
+            // Kullanıcı üst üste hatalı bilgileri 3 defa girebilsin. 3 defa hatalı giriyorsa hakkınız bitti mesajı vereceğiz.
+            // Doğru bilgiyi girdiyse hoşgeldin diyeceğiz.
+
+            {
+                string KULLANICIADI = "admin";
+                string SIFRE = "123";
+                int DENEME_HAKKI = 3;
+                int deneme = 0;
+                string kul_adı, kul_sifre;
+
+                while (deneme < DENEME_HAKKI)
+                {
+                    Console.Write("Kullanıcı adı girin = ");
+
+                    kul_adı = Console.ReadLine();
+
+                    Console.Write("Şifreyi girin = ");
+
+                    kul_sifre = Console.ReadLine();
+
+                    if (kul_adı == KULLANICIADI && kul_sifre == SIFRE)
+                    {
+                        Console.WriteLine("Hoşgeldiniz");
+                        break;
+                    }
+                    Console.WriteLine("Yanlış kullanıcı adı veya şifre girdiniz.");
+                    deneme++;
+                    Console.Write("Kalan hakkınız ");
+                    Console.WriteLine(DENEME_HAKKI - deneme);
+
+
+                }
+                Console.ReadKey();
+            }
+ }     }    }
+
+
+
+
+
+
+
+
+
+
+
+       
+    
+
+      
---
+++ Odev4Deneme2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Odev4Deneme2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Kullanıcıdan kullanıcı adı ve şifre isteyelim. Kullanıcı Adı => Admin && Şifre => 123 , Eğer kullanıcı yanlış girerse bu bilgileri tekrar isteyelim.
+            // Kullanıcı üst üste hatalı bilgileri 3 defa girebilsin. 3 defa hatalı giriyorsa hakkınız bitti mesajı vereceğiz.
+            // Doğru bilgiyi girdiyse hoşgeldin diyeceğiz.
+
+            string kul_adi = "admin";
+            string sifre = "123";
+
+            int n = 0;
+            int j = 0;
+            int deneme = 0;
+
+            while (n <= 2)
+            {
+                Console.Write("Lütfen kullanıcı adınızı girin=");
+                string kullaniciadi = Console.ReadLine();
+
+                Console.Write("Lütfen Şifrenizi Giriniz=");
+                string kullanici_sifre = Console.ReadLine();
+
+                if (kul_adi == kullaniciadi)
+                {
+
+                    Console.WriteLine("Hoşgeldin");
+                }
+                else
+
+                {
+                    n++;
+                    Console.WriteLine("Yanlış Girdiniz Lütfen Tekrar Giriniz");
+                    continue;
+                    
+                }                       
+                          
+
+
+
+
+         }   }
+ }   }
